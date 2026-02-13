f9c229744775fa9fbdb21a0d5814034310f8d75c
vishwanath <vshwanathvishu@gmail.com>
2026-02-13 13:28:32 +0530

    git changes

--- src/components/headers.js
+++ src/components/headers.js
@@ -1,34 +1,46 @@
 'use client'
 import React from "react";
 import Link from "next/link";
+import { useEffect, useState } from "react";
+import Image from "next/image";
+import {logo} from '../../public/images/logo.png'
 const Headers = () => {
-  const token = localStorage.getItem("token");
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const storedToken = localStorage.getItem("token");
+    setToken(storedToken);
+  }, []);
 
   return (
-    <div className="flex justify-between px-6 py-4 bg-green-600 text-white items-center">
-      <h1>RT</h1>
-      <div className="flex gap-4">
-        <Link href={"/"}>Home</Link>
-        <Link href={"/about"}>About us</Link>
-        <Link href={"/products"}>Products</Link>
-        <Link href={"/contact"}>Contact</Link>
-      </div>
-      <div>
-        {!token ? (
-          <Link href={"/login"}>Login</Link>
-        ) : (
-          <div
-            onClick={() => {
-              localStorage.removeItem("token");
-              window.location.reload();
-            }}
-            className="cursor-pointer"
-          >
-            Logout
+    <>
+      {token && (
+        <div className="flex justify-between px-6 py-4 bg-green-600 text-white items-center">
+         <Image src={logo} width={100} height={50} alt="Logo" />
+          <div className="flex gap-4">
+            <Link href={"/"}>Home</Link>
+            <Link href={"/about"}>About us</Link>
+            <Link href={"/products"}>Products</Link>
+            <Link href={"/contact"}>Contact</Link>
           </div>
-        )}
-      </div>
-    </div>
+          <div>
+            {!token ? (
+              <Link href={"/login"}>Login</Link>
+            ) : (
+              <div
+                onClick={() => {
+                  localStorage.removeItem("token");
+                  window.location.reload();
+                }}
+                className="cursor-pointer"
+              >
+                Logout
+              </div>
+            )}
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
